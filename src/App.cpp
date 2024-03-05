cbe5bd1d61a338c58131bc66f2941c95f010b2c5
Renée Rulkens <42935816+ReneeR33@users.noreply.github.com>
2024-03-05 15:14:33 +0100

    Removed comments, test scene

--- src/App.cpp
+++ src/App.cpp
@@ -13,7 +13,6 @@
 #include "Components/Puzzle.hpp"
 #include "primitives.hpp"
 #include "ModelLoader.hpp"
-#include <iostream>
 
 #define WINDOW_WIDTH 1500
 #define WINDOW_HEIGHT 950
@@ -26,33 +25,7 @@ void App::run() {
     Renderer renderer;
     PuzzleViewSystem::init(scene, window);
 
-    //initTestScene();
     initExplodedViewTestScene();
-
-    //---------------------------------------
-
-    /*auto background = scene.create();
-    scene.emplace<Background>(background, glm::vec3(0.05f));
-
-    loader.LoadSolution("resources/data/puzzle_test.json");
-
-    auto dirLight = scene.create();
-    scene.emplace<DirLight>(dirLight, 
-        glm::vec3(0.0f, 0.14f, -1.0f),
-        glm::vec3(1.0f),
-        glm::vec3(1.0f),
-        glm::vec3(1.0f)
-    );
-
-    auto camera = scene.create();
-    scene.emplace<Camera>(camera,
-        glm::vec3(0.0f, 1.0f, 4.0f),
-        glm::vec3(0.0f, 0.0f, -1.0f),
-        glm::vec3(0.0f, 1.0f, 0.0f),
-        0.1f, 100.0f, 80.0f
-    );*/
-
-    //----------------------------------------
 
     renderer.load(scene);
     DebugWindow debugWindow(window);
@@ -66,75 +39,6 @@ void App::run() {
 
         window.update();
     }
-}
-
-void App::initTestScene() {
-    auto background = scene.create();
-    scene.emplace<Background>(background, glm::vec3(0.05f));
-
-    auto dirLight = scene.create();
-    scene.emplace<DirLight>(dirLight,
-                            glm::vec3(0.0f, 0.14f, -1.0f),
-                            glm::vec3(1.0f),
-                            glm::vec3(1.0f),
-                            glm::vec3(1.0f)
-    );
-
-    auto camera = scene.create();
-    scene.emplace<Camera>(camera,
-                          glm::vec3(0.0f, 1.0f, 4.0f),
-                          glm::vec3(0.0f, 0.0f, -1.0f),
-                          glm::vec3(0.0f, 1.0f, 0.0f),
-                          0.1f, 100.0f, 80.0f
-    );
-
-    auto cube_1 = scene.create();
-    scene.emplace<Model>(cube_1, primitives::cube);
-    scene.emplace<Shader>(cube_1, "shaders/shader.vert", "shaders/shader.frag");
-    scene.emplace<Material>(cube_1,
-                            glm::vec3(1.0f, 0.0f, 0.0f),
-                            glm::vec3(0.1f, 0.0f, 0.0f),
-                            glm::vec3(0.0f),
-                            1.0f
-    );
-    scene.emplace<Transform>(cube_1,
-                             glm::vec3(1.5f, 2.0f, 0.0f),
-                             glm::vec3(10.0f),
-                             glm::vec3(1.0f, 1.0f, 1.0f)
-    );
-    scene.emplace<PuzzlePiece>(cube_1, glm::vec3(1.5f, 2.0f, 0.0f));
-
-    auto cube_2 = scene.create();
-    scene.emplace<Model>(cube_2, primitives::cube);
-    scene.emplace<Shader>(cube_2, "shaders/shader.vert", "shaders/shader.frag");
-    scene.emplace<Material>(cube_2,
-                            glm::vec3(0.0f, 1.0f, 0.0f),
-                            glm::vec3(0.0f, 0.1f, 0.0f),
-                            glm::vec3(0.0f),
-                            1.0f
-    );
-    scene.emplace<Transform>(cube_2,
-                             glm::vec3(1.5f, 0.0f, -1.0f),
-                             glm::vec3(10.0f),
-                             glm::vec3(1.0f, 1.0f, 1.0f)
-    );
-    scene.emplace<PuzzlePiece>(cube_2, glm::vec3(1.5f, 0.0f, -1.0f));
-
-    auto cube_3 = scene.create();
-    scene.emplace<Model>(cube_3, primitives::cube);
-    scene.emplace<Shader>(cube_3, "shaders/shader.vert", "shaders/shader.frag");
-    scene.emplace<Material>(cube_3,
-                            glm::vec3(0.0f, 0.0f, 1.0f),
-                            glm::vec3(0.0f, 0.0f, 0.1f),
-                            glm::vec3(0.0f),
-                            1.0f
-    );
-    scene.emplace<Transform>(cube_3,
-                             glm::vec3(3.5f, 0.5f, -1.0f),
-                             glm::vec3(10.f),
-                             glm::vec3(1.0f, 1.0f, 1.0f)
-    );
-    scene.emplace<PuzzlePiece>(cube_3, glm::vec3(3.5f, 0.5f, -1.0f));
 }
 
 void App::initExplodedViewTestScene() {
